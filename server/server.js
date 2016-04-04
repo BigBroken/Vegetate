6797261dba7389d2c829084aab13a482cf88c6bb
Kenneth Montgomery <kennethMontgo@gmail.com>
2016-04-04 16:08:08 -0700

    Add get handler for pantry db request

--- server/server.js
+++ server/server.js
@@ -13,6 +13,17 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/../App'));
 
+app.get('/db/pantry',function(res, req){
+  console.log('attempting to get pantry');
+  model.Produce.find({}, function(err, pantry){
+    if(err){
+      console.log(err);
+      return err;
+    }
+    res.send(pantry);
+  });
+});
+
 
 app.listen(port, function(err) {
   if (err) {
