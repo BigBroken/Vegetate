18775ffad626095a930cc7e2c6a26d00119eb3d3
Kenneth Montgomery <kennethMontgo@gmail.com>
2016-04-04 14:31:25 -0700

    Enable mongoose connection

--- server/server.js
+++ server/server.js
@@ -3,11 +3,11 @@ var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 
 var port = process.env.PORT || 8000;
-var db = process.env.MONGOLAB_URI || 'mongodb://localhost/';
+var db = process.env.MONGOLAB_URI || 'mongodb://localhost/produce';
 
 var app = express();
 
-// mongoose.connect(db);
+mongoose.connect(db);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
