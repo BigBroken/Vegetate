b270a8624157eadebc48bcea7b193153eb3c4415
Kenneth Montgomery <kennethMontgo@gmail.com>
2016-04-04 12:22:14 -0700

    add index html and angular module app.js

---
+++ app/app.js
@@ -0,0 +1,9 @@
+angular.module('vegetate', [
+  'ngRoute',
+  'myApp.view1',
+  'myApp.view2',
+  'myApp.version'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.otherwise({redirectTo: '/view1'});
+}]);
