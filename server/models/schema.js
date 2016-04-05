5286f130e52bc032abb81ab190d7fe36a5fa7fc4
Kenneth Montgomery <kennethMontgo@gmail.com>
2016-04-04 23:04:47 -0700

    Add ability to generate database seed Data

--- server/models/schema.js
+++ server/models/schema.js
@@ -1,17 +1,42 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var stuff = require('../../data/produce.js')
+var _ = require('underscore');
 
 var produceSchema = new Schema({
   name:  String,
   shelf_life: Number,
-  date: { type: Date, default: Date.now },
 });
+exports.Produce = mongoose.model('produce', produceSchema);
 
 var userSchema = new Schema({
   name: String,
   password: String,
-
 });
+exports.User = mongoose.model('user', userSchema);
 
-exports.User = mongoose.model('User', userSchema);
-exports.Produce = mongoose.model('Produce', produceSchema);
+var pantrySchema = new Schema({
+  name: [
+    {type: Schema.Types.ObjectId, ref:'produce'}
+  ],
+  date: { type: Date, default: Date.now },
+  shelf_life:[
+    {type: Schema.Types.ObjectId, ref:'produce'}
+  ]
+});
+exports.Pantry = mongoose.model('pantry', pantrySchema);
+
+// populate the server
+// console.log(data); 
+// _.each(stuff.data, function(produceObj, key , list) {
+//   new exports.Produce({name:produceObj.name, shelf_life:produceObj.shelf_life}).save(function(err,produce){
+//     console.log(produce);
+//   });
+// });
+
+// exports.Produce.find({}, function(err,produce){
+//   if(err){
+//     return
+//   }
+//   console.log(produce);
+// });
